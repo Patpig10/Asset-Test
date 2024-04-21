06be866cb268f42f159cba78c52d96dc6b917aa5
Mickname342 <michele.marchese.andreu@gmail.com>
2024-04-21 18:38:52 +0100

    Added pickup neccessities for wire minigmae
    Tape and key are in the overwold to pickup and are necessary to complete the task but don't work yet

--- Assets/Minigames/Tape_Check.cs
+++ Assets/Minigames/Tape_Check.cs
@@ -13,27 +13,37 @@ public class Tape_Check : MonoBehaviour
     void Start()
     {
         rectTransform = GetComponent<RectTransform>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         if(rectTransform.localPosition.x < 25 && rectTransform.localPosition.x > -20 &&
-            rectTransform.localPosition.y < 50 && rectTransform.localPosition.y > 6)
+            rectTransform.localPosition.y < 50 && rectTransform.localPosition.y > 6 && !dragged)
         {
-                fin.Invoke();
-            finished = true;
+            StartCoroutine(Coroutine1());
         }
-        /*else if (rectTransform.position.x < 640 && rectTransform.position.x > 580 &&
-           rectTransform.position.y < 430 && rectTransform.position.y > 350)
+        if (Input.GetKey(KeyCode.Mouse0))
         {
-            fin.Invoke();
-        }*/
-        print(rectTransform.localPosition);
+            dragged = true;
+        }
+        else
+        {
+            dragged = false;
+        }
     }
 
     public void Dragging()
     {
-        dragged = !dragged;
+        
+    }
+
+    IEnumerator Coroutine1()
+    {
+        yield return new WaitForSeconds(0.4f);
+        fin.Invoke();
+        finished = true;
+
     }
 }
